cbdb7a6af3b585c733141054752dae6357830faa
thewr <36010296+thewr@users.noreply.github.com>
2018-03-15 10:35:15 -0400

    Update BQUEUE_CPP

--- BQUEUE.cpp
+++ BQUEUE.cpp
@@ -1,5 +1,4 @@
 #include "BQUEUE.h"
-#include <iostream>
 
 using namespace std;
 
@@ -7,31 +6,83 @@ using namespace std;
 BQUEUE::BQUEUE() 
 {
 	cout << "Default Constructor Envoked!" << endl;
-	front = 0;
+	//intialize empty que
+	front->next = front;//0; //front->prev == back
+	front->prev = front;//0;
+	front = 0; 
 }
 
 
 BQUEUE::~BQUEUE()
 {
-	
+	//cout << "~QUEUE has been called\n";
+	//while (!Empty())
+	//{
+	//	deQueue();
+	//}
 }
 
 BQUEUE::BQUEUE(const BQUEUE & B)
 {
-
+	//if (Empty())
+	//{
+	//	front = back = new Qnode;
+	//	front->data = item;
+	//}
+	//else
+	//{
+	//	back->next = new bqnode;
+	//	back->next->prev = back;
+	//	back = back->next;
+	//	back->data = item;
+	//}
 }
 
 void BQUEUE::Enqueue(int) 
 {
-
+	//if (Empty())
+	//{
+	//	front = back = new bqnode;
+	//	front->data = item;
+	//}
+	//else
+	//{ 
+	//	back->next = new bqnode;
+	front->prev->next = new bqnode;
+	//	back->next->prev = back;
+	//	back = back->next;
+	//	back->data = item;
+	//}
 }
 
 void BQUEUE::Dequeue() 
 {
+	//if (!Empty())
+	//{
+	//	bqnode *p = front;
+	//	front = front->next;
+	//	if (front != 0)
+	//		front->prev = 0;
+	//	else
+	//		back = 0;
 
+	//	delete p;
+	//}
+	//else
+	//{
+	//	cout << "Cannot deQueue because queue is empty\n";
+	//}
 }
 
 void BQUEUE::Print()
 {
+	//bqnode *p = front;
+	////bqnode *p = back;
 
+	//while (p != 0)
+	//{
+	//	cout << p->data << endl;
+	//	p = p->next;
+	//	//p=p->prev;
+	//}
 }
