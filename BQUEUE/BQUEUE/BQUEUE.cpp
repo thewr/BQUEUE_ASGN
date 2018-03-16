2a88645857a938ba9482748c73a66e60937259dd
thewr <36010296+thewr@users.noreply.github.com>
2018-03-16 12:06:07 -0400

    update

--- BQUEUE/BQUEUE/BQUEUE.cpp
+++ BQUEUE/BQUEUE/BQUEUE.cpp
@@ -3,23 +3,24 @@
 using namespace std;
 
 
-BQUEUE::BQUEUE() 
+BQUEUE::BQUEUE()
 {
 	cout << "Default Constructor Envoked!" << endl;
 	//intialize empty que
-	front->next = front;//0; //front->prev == back
-	front->prev = front;//0;
-	front = 0; 
+	//front->prev = front;
+	front = 0;
+	//front->next = front;//0; //front->prev == back
+	//front->prev = front;//0;	
 }
 
 
 BQUEUE::~BQUEUE()
 {
 	cout << "~QUEUE has been called\n";
-	while (!Empty())
-	{
-		Dequeue();
-	}
+	//while (!Empty())
+	//{
+	ClearQueue();
+	//}
 }
 
 BQUEUE::BQUEUE(const BQUEUE & B)
@@ -38,48 +39,63 @@ BQUEUE::BQUEUE(const BQUEUE & B)
 	//}
 }
 
-void BQUEUE::Enqueue(int) 
+void BQUEUE::Enqueue(int item)
 {
 	if (Empty())
 	{
-	front = new bqnode; //front = back = new bqnode;
-	front->next = front;
-	front->prev = front;
-	front->data = item; //front->data = item;
+		front = new bqnode; 
+		front->prev = new bqnode;//new bqnode;//front;//new bqnode;
+		front->next = 0;
+		front->prev = 0;
+		front->time = item; //front->data = item;
 	}
 	else
-	{ 
-	front->prev->next = new bqnode;  //back->next = new bqnode;
-	front->prev->next->prev = front->prev; //back->next->prev = back;
-	front->prev=front->prev->next; //back = back->next;
-	front->prev->data=item;  //back->data = item;
+	{
+		front->prev->next = new bqnode;  //back->next = new bqnode;
+		front->prev->next->prev = front->prev; //back->next->prev = back;
+		front->prev = front->prev->next; //back = back->next;
+		front->prev->time = item;  //back->data = item;
 	}
 }
 
-void BQUEUE::Dequeue() 
+void BQUEUE::Dequeue()
 {
 	if (!Empty())
 	{
-		bqnode *p = front;
-		front = front->next;
-		if (front != 0)
-			front->prev = 0;
+			bqnode *p = front;
+			front = front->next;
+			if (front != 0)
+				front->prev = 0;
+			//else
+			//	front->prev = 0; //back = 0;
+			delete p;
+		}
 		else
-			front->prev = 0; //back = 0;
-		delete p;
+		{
+			cout << "Cannot Dequeue because queue is empty\n";
+		}
+}
+
+void BQUEUE::ClearQueue()
+{
+	if (!Empty())
+	{
+		bqnode *p, *tmp;
+		p = front->next;
+		while (p != 0)
+		{
+			tmp = p;
+			p = p->next;
+			delete tmp;
+		}
+		front->next = 0;
+		front->prev = front;
+		//Count = 0;
 	}
 	else
 	{
 		cout << "Cannot Dequeue because queue is empty\n";
 	}
-	
-	////delete from front
-	//bqnode *p = front;
-	//front = p -> next;
-	//front -> prev = p-> prev;
-	//p->prev->next = front;
-	//delete p;
-	
 }
 
 void BQUEUE::Print()
@@ -89,7 +105,7 @@ void BQUEUE::Print()
 
 	while (p != 0)
 	{
-		cout << p->data << endl;
+		cout << p->time << endl;
 		p = p->next;
 		//p=p->prev;
 	}
